f6e50a56d108c9c4374ec47462a963a3c9c4df1e
kolserdav <serega12101983@gmail.com>
2022-01-21 09:34:21 +0700

    Added read stream

--- client/assets/js/script.js
+++ client/assets/js/script.js
@@ -1,9 +1,24 @@
 // @ts-check
 const SOCKET_SERVER = 'https://webcall.loc';
 
+const mediaSource = new MediaSource();
+let sourceBuffer = null;
+let arrayOfBlobs = [];
+function appendToSourceBuffer() {
+  if (mediaSource.readyState === 'open' && sourceBuffer && sourceBuffer.updating === false) {
+    sourceBuffer.appendBuffer(arrayOfBlobs.shift());
+  }
+
+  // Limit the total buffer size to 20 minutes
+  // This way we don't run out of RAM
+  if (video.buffered.length && video.buffered.end(0) - video.buffered.start(0) > 1200) {
+    sourceBuffer.remove(0, video.buffered.end(0) - 1200);
+  }
+}
+
 navigator.getUserMedia =
   navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
+let start = false;
 window.onload = () => {
   const socket = io();
   socket.on('connect', () => {
@@ -13,21 +28,27 @@ window.onload = () => {
         function (stream) {
           // use MediaStream Recording API
           const recorder = new MediaRecorder(stream);
-
           // fires every one second and passes an BlobEvent
           recorder.ondataavailable = (event) => {
+            console.log(event);
             // get the Blob from the event
             const blob = event.data;
-            socket.send(blob);
+            socket.emit('write stream', blob);
           };
-
-          // make data available event fire every one second
-          recorder.start(1000);
+          const url = URL.createObjectURL(mediaSource);
           const video = document.querySelector('video');
-          video.srcObject = stream;
+          video.src = url;
+          mediaSource.addEventListener('sourceopen', () => {
+            console.log(open);
+            sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="opus,vp8"');
+            sourceBuffer.addEventListener('updateend', appendToSourceBuffer);
+          });
           video.onloadedmetadata = function (e) {
+            console.log(32);
             video.play();
           };
+          // make data available event fire every one second
+          recorder.start(1000);
         },
         function (err) {
           console.log('The following error occurred: ' + err.name);
@@ -39,6 +60,9 @@ window.onload = () => {
   });
   socket.on('disconnect', () => {
     console.log(2, socket.connected);
+  });
+  socket.on('read stream', (str) => {
+    arrayOfBlobs.concat(str);
   });
   socket.on('connect_error', () => {
     setTimeout(() => {
--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const server = require('http').createServer();
 const uuidV4 = require('uuid').v4;
 const SocketServer = require('socket.io').Server;
 const io = new SocketServer(server);
+const fs = require('fs');
+const path = require('path');
+
+const DATA_PATH = path.resolve(__dirname, '../data');
 
 const app = express();
 
@@ -14,9 +18,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/data', express.static(path.resolve(__dirname, '../data')));
 
 app.get('/join', (req, res) => {
   res.send({ link: uuidV4() });
+});
+const oggFile = `${DATA_PATH}/stream.webm`;
+app.get('/stream', (req, res) => {
+  console.log(322);
+  const stream = fs.createReadStream(oggFile);
+  stream.pipe(res);
 });
 
 server
@@ -29,8 +40,15 @@ server
 
 io.on('connection', (socket) => {
   console.log('socket established');
-  socket.on('message', (msg) => {
-    console.log(msg);
+
+  try {
+    fs.unlinkSync(oggFile);
+  } catch (e) {}
+  const writeStream = fs.createWriteStream(oggFile);
+  socket.on('write stream', (msg) => {
+    writeStream.write(msg);
+    const { bytesWritten } = writeStream;
+    socket.emit('read stream', bytesWritten);
   });
 });
 io.on('connect', () => {
